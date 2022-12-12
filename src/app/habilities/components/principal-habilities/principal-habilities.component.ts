c0036608c3f39cad4ae7c86b06a0ee4314a9bc29
Juan Bohigues <jabohigues@iesfuentesanluis.org>
2022-12-12 19:14:13 +0100

    Download images and clean grid habilities

--- src/app/habilities/components/principal-habilities/principal-habilities.component.ts
+++ src/app/habilities/components/principal-habilities/principal-habilities.component.ts
@@ -9,7 +9,15 @@ export class PrincipalHabilitiesComponent implements OnInit {
   habilities = [
     { title: 'Angular', img: 'logoAngular' },
     { title: 'TypeScript', img: 'logoTypescript' },
+    { title: 'JavaScript', img: 'logoJS' },
+    { title: 'HTML', img: 'logoHTML' },
+    { title: 'CSS', img: 'logoCSS' },
+    { title: 'jQuery', img: 'logoJQuery' },
     { title: 'Node.js', img: 'logoNodejs' },
+    { title: 'Express', img: 'logoExpress' },
+    { title: 'Prisma', img: 'logoPrisma' },
+    { title: 'Java', img: 'logoJava' },
+    { title: 'MySQL', img: 'logoMySQL' },
   ];
 
   constructor() {}
