02fd8f2deb003d41ad9dad21c874942fe249104d
Juan Bohigues <jbohigues@prode_domi.es>
2023-05-16 21:19:25 +0200

    done responsive habilities section

--- src/app/habilities/habilities.module.ts
+++ src/app/habilities/habilities.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrincipalHabilitiesComponent } from './components/principal-habilities/principal-habilities.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [PrincipalHabilitiesComponent],
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatTooltipModule],
   exports: [PrincipalHabilitiesComponent],
 })
 export class HabilitiesModule {}
