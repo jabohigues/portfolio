66bf5af8c583f1708365ebd1189d66ca1264f4ae
Juan Bohigues <jbohigues@prode_domi.es>
2023-05-16 21:27:09 +0200

    done responsive tecnologies section

--- src/app/tecnologies/tecnologies.module.ts
+++ src/app/tecnologies/tecnologies.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrincipalTecnologiesComponent } from './components/principal-tecnologies/principal-tecnologies.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [PrincipalTecnologiesComponent],
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatTooltipModule],
   exports: [PrincipalTecnologiesComponent],
 })
 export class TecnologiesModule {}
