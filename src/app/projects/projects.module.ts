27a373096f69f2d54b9df9d0f3f3dae3f1312542
Juan Bohigues <jbohigues@prode_domi.es>
2023-05-24 22:28:59 +0200

    improving responsive design

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { PrincipalProjectsComponent } from './components/principal-projects/principal-projects.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [PrincipalProjectsComponent],
-  imports: [CommonModule, MatCardModule, MatChipsModule],
+  imports: [CommonModule, MatCardModule, MatChipsModule, MatDividerModule],
   exports: [PrincipalProjectsComponent],
 })
 export class ProjectsModule {}
