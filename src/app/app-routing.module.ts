b7050c28c1baf7d43ed2eefcc3947088cddba26b
Juan Bohigues <72445969+jbohigues@users.noreply.github.com>
2024-08-06 11:42:47 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './home/components/principal/principal.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: PrincipalComponent },
-  { path: '**', redirectTo: 'home' }, // Wildcard route for a 404 page https://angular.io/guide/router
+  { path: '', component: PrincipalComponent },
+  { path: '**', redirectTo: '' }, // Wildcard route for a 404 page https://angular.io/guide/router
 ];
 
 @NgModule({
